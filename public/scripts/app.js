da6c39c945478191fe8a2d4510faaa9a7bab4a3c
Natalie Mok <nataliemok__@hotmail.com>
2016-09-22 00:19:42 +0000

    Added a button to slide the tweet composer'

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,30 +19,65 @@ tweetData = {
   "created_at": 1461116232227
 }
 
+$(function focusTextBox() {
+  $("button").on("click", function(e) {
+    e.preventDefault();
+    $("section.new-tweet").slideToggle("slow", function() {
+       $("textarea").focus();
+       $("section.allTweets");
+    });
+  });
+});
 
-$(function() {
+
+$(function renderTweets() {
   $("form input").on("click", function(e) {
     e.preventDefault();
-    let $tweet = $("<article>").addClass("tweet");
-    let $header = $("<header>");
-    let $img = $("<img>").attr("src", tweetData.user.avatars.small).addClass("avatarPics");
-    let $handle = $("<p>").addClass("handle").text(tweetData.user.handle);
-    let $name = $("<p>").addClass("name").text(tweetData.user.name);
-    $header.append($img).append($handle).append($name);
 
-    let $tweetContainer = $("<div>").addClass("actualTweet");
-    let $realTweet = $("<p>").text(tweetData.content.text);
+    const counter = $(".counter");
 
-    $tweetContainer.append($realTweet);
+    if (counter.text() < 0) {
+      alert("Your tweet is too long...");
+    } else if (counter.text() == 140) {
+      alert("You can't hum about nothing!");
+    } else {
+      let $tweet = $("<article>").addClass("tweet");
+      let $header = $("<header>");
+      let $img = $("<img>").attr("src", tweetData.user.avatars.small).addClass("avatarPics");
+      let $handle = $("<p>").addClass("handle").text(tweetData.user.handle);
+      let $name = $("<p>").addClass("name").text(tweetData.user.name);
+      $header.append($img).append($handle).append($name);
 
-    let $footer = $("<footer>");
-    let $date = $("<p>").text(tweetData.created_at);
+      let $tweetContainer = $("<div>").addClass("actualTweet");
+      let $realTweet = $("<p>").text(document.getElementById('theTweet').value);
 
-    $footer.append($date);
+      $tweetContainer.append($realTweet);
 
-    $tweet.append($header).append($tweetContainer).append($footer);
+      let $footer = $("<footer>");
+      let $date = $("<p>").text(tweetData.created_at);
 
-    $('.allTweets').append($tweet);
+      $footer.append($date);
+
+      $tweet.append($header).append($tweetContainer).append($footer);
+
+      $('.allTweets').prepend($tweet);
+
+      $("form textarea").val("");
+    }
   });
 
+  $(function loadTweets() {
+    const $button = $("input");
+    $button.on("click", function (e) {
+      e.preventDefault();
+      $.ajax({
+        url: "/tweets",
+        method: "GET",
+        success: function (showTweets) {
+          console.log("Success: ", showTweets);
+        },
+        datatype: "JSON"
+      });
+    });
+  });
 });
